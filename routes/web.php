fc8529bbe9287855e9bbd1b1811257d41364a081
Meng Wei <yyy333567@163.com>
2019-09-19 08:31:32 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@
 //我们向 http://weibo.test/ 发出了一个请求，
 //则该请求将会由 StaticPagesController 的 home 方法进行处理
 //http://weibo.test
-Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 //http://weibo.test/help
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 //http://weibo.test/about
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
