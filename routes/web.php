6115067403f0bede1508e4764e4caf7ad76370f1
Meng Wei <yyy333567@163.com>
2019-09-19 11:26:00 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 //http://weibo.test/about
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
+
