39623468f2482f981e361c3496907b8a8c50356e
Meng Wei <yyy333567@163.com>
2019-09-20 04:41:09 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-         if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            //Auth::user() 方法来获取 当前登录用户 的信息
            return redirect()->route('users.show', [Auth::user()]);
