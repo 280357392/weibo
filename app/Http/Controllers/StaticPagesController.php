f27b530a798d7e34955f084e6f084732113de37f
Meng Wei <yyy333567@163.com>
2019-09-25 14:53:31 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 //命令生成：$ php artisan make:controller StaticPagesController
 class StaticPagesController extends Controller
 {
     //http://weibo.test
-    public function home(){
+    public function home()
+    {
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
         //第一个参数是视图的路径名称，第二个参数是与视图绑定的数据，第二个参数为可选。
-        return view('static_pages/home');
+        return view('static_pages/home', compact('feed_items'));
     }
 
     //http://weibo.test/help
